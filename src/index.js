64a9d747a81196de46620550610761985738b4bc
Aben25 <51212800+Aben25@users.noreply.github.com>
2022-08-30 12:35:37 +0000

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <HashRouter>
     <App />
+  </HashRouter>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
