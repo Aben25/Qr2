60e0a96a1c57c60abb14249ab096101e970de5f9
Abenezer Nuro <abenuro@gmail.com>
2022-08-30 02:20:07 -0400

    fixed_bugs

--- src/App.js
+++ src/App.js
@@ -33,8 +33,6 @@ function App() {
 
   useEffect(() => {
     window.sessionStorage.setItem("currentEvent_1", JSON.stringify(currentEvent));
-    console.log('============id========================');
-    console.log(currentEvent.EventUniqueId);
 
     const url = `https://connect.artba.org/api/registrations?eventId=${currentEvent.EventUniqueId===undefined?"16763198-b512-4fc8-ac56-02c29a75c902":currentEvent.EventUniqueId}`
  
